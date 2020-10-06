70e15f52c378572c14c2857a35d224a1f9b421ce
Vladimir Chuikov <python46dev@gmail.com>
2020-10-07 01:18:30 +0400

    Add filters for status and expected_dead_line.

--- app/task_app/views.py
+++ app/task_app/views.py
@@ -1,22 +1,17 @@
 from rest_framework import viewsets
 from .serializers import *
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
-
 from django.contrib.auth import authenticate
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
     HTTP_200_OK
 )
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class UserLogin(APIView):
@@ -43,6 +38,8 @@ class TaskUserView(viewsets.ModelViewSet):
 
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'expected_dead_line']
 
     def get_queryset(self):
         queryset = Task.objects.filter(user=self.request.user)
