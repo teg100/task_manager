fbf32021cff809982d174f735fbb6058f29da98b
Vladimir Chuikov <python46dev@gmail.com>
2020-10-06 21:19:48 +0400

    add user field in model Task. Add user in serializer TaskSerializer.

--- app/task_app/models.py
+++ app/task_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
@@ -15,3 +16,8 @@ class Task(models.Model):
     date_create = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=40, choices=STATUS_CHOICES)
     expected_dead_line = models.DateField(blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+
+    def __str__(self):
+        return self.title
+
--- app/task_app/serializers.py
+++ app/task_app/serializers.py
@@ -6,4 +6,4 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Task
-        fields = ['title', 'description', 'date_create', 'status', 'expected_dead_line']
+        fields = ['title', 'description', 'date_create', 'status', 'expected_dead_line', 'user']
