64ac84708cba7e5bfe0e684c72988d50be487471
Vladimir Chuikov <python46dev@gmail.com>
2020-10-06 20:20:55 +0400

    create Task model

--- app/task_app/models.py
+++ app/task_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('NW', 'New'),
+        ('PL', 'Planned'),
+        ('IW', 'In work'),
+        ('FS', 'Finished'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_create = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES)
+    expected_dead_line = models.DateField(blank=True, null=True)
