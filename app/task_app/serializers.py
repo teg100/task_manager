fd8aa2ca707bd185702657fd7a3a695ad6668b6d
Vladimir Chuikov <python46dev@gmail.com>
2020-10-06 21:44:50 +0400

    Add url field to TaskSerializer.

--- app/task_app/serializers.py
+++ app/task_app/serializers.py
@@ -6,4 +6,5 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Task
-        fields = ['title', 'description', 'date_create', 'status', 'expected_dead_line', 'user']
+        fields = ['url', 'title', 'description', 'date_create', 'status', 'expected_dead_line']
+
