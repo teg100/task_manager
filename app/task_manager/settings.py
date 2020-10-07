211edec628eca01ecf04933eca2162b85b2be092
Vladimir Chuikov <python46dev@gmail.com>
2020-10-07 04:00:19 +0400

    Add in  settings.py STATIC_ROOT for doc.

--- app/task_manager/settings.py
+++ app/task_manager/settings.py
@@ -66,6 +66,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication', ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
